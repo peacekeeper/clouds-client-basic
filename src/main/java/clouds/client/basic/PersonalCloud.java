3fa2752ec1284a511c18757683b1d372033445b9
animeshc <animesh.chowdhury@gmail.com>
2013-10-15 17:58:47 -0400

    changes

--- src/main/java/clouds/client/basic/PersonalCloud.java
+++ src/main/java/clouds/client/basic/PersonalCloud.java
@@ -2097,9 +2097,9 @@ public class PersonalCloud {
 			buf.append("</input>");
 			buf.append("<input type=\"hidden\" name=\"successurl\" value=\"http://acme.respectnetwork.net/demo-acme-site/acs\">");
 			buf.append("</input>");
-			buf.append("<input type=\"hidden\" name=\"connectRequest\" value=\""); 
+			buf.append("<input type=\"hidden\" name=\"connectRequest\" value=\'"); 
 			buf.append(respectConnectRequest);
-			buf.append("\">");
+			buf.append("\'>");
 			buf.append("</input>");
 			buf.append("Your Secret Token: <input type=\"text\" name=\"secrettoken\"/><br>");
 			buf.append("<input type=\"submit\" value=\"Authenticate!\"/>");
